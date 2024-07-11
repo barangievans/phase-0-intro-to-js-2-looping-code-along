1e4f272e7361e4f94fd3cdec9d480a6a91f4e1fb
barangievans <barangievans@gmail.com>
2024-07-11 11:46:18 +0300

    code-along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// index.js
+
+// Function to generate thank you messages
+function writeCards(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+  
+  // Function to count down from a number to zero
+  function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+  
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
